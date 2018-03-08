f62d2752d0795bc843fdf7ed7cf3a88dd8b4554d
sean <S.ritchie577@gmail.com>
2018-03-08 15:52:33 -0800

    Finished most of it, still need to comment and change a few things

--- src/Project2/fractionalknap/InsertionSort.java
+++
@@ -1,7 +0,0 @@
-package Project2.fractionalknap;
-
-public class InsertionSort {
-    static int[][] sort(int[][]x, int n){
-        return x;
-    };
-}
--- src/Project2/fractionalknap/main.java
+++ src/Project2/fractionalknap/main.java
@@ -1,36 +1,57 @@
 package Project2.fractionalknap;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args){
-        int capacity;
-        int numberOfItems;
-        int weight;
-        int cost;
-        int[][] items;
+    public static void main(String args[]) throws IOException
+    {
+        int i,j=0,max_qty,m,n;
+        float sum=0,max;
+        Scanner sc = new Scanner(System.in);
+        int array[][];
+        System.out.println("Enter number of items");
+        n=sc.nextInt();
+        array = new int[2][n];
+        System.out.println("Enter the weights of each items");
+        for(i=0;i<n;i++)
+            array[0][i]=sc.nextInt();
 
-        System.out.println("What weight capacity would you like the knapsack to be? ");
-        Scanner scanner = new Scanner(System.in);
-        capacity = scanner.nextInt();
+        System.out.println("Enter the values of each items");
+        for(i=0;i<n;i++)
+            array[1][i]=sc.nextInt();
 
-        System.out.println("How many items would you like to use? ");
-        numberOfItems = scanner.nextInt();
+        System.out.println("Enter maximum volume of knapsack :");
+        max_qty=sc.nextInt();
 
-        items = new int[2][numberOfItems];
-        System.out.println("Enter the weights of each item: ");
-        for(int i = 1; i < numberOfItems; i++){
-            items[0][i] = scanner.nextInt();
+        m=max_qty;
+        while(m>=0)
+        {
+            max=0;
+            for(i=0;i<n;i++)
+            {
+                if(((float)array[1][i])/((float)array[0][i])>max)
+                {
+                    max=((float)array[1][i])/((float)array[0][i]);
+                    j=i;
+                }
+            }
+            if(array[0][j]>m)
+            {
+                System.out.println("Quantity of item number: " +  (j+1) + " added is " +m);
+                sum+=m*max;
+                m=-1;
+            }
+            else
+            {
+                System.out.println("Quantity of item number: " + (j+1) + " added is " + array[0][j]);
+                m-=array[0][j];
+                sum+=(float)array[1][j];
+                array[1][j]=0;
+            }
         }
-
-        System.out.println("Enter the cost of each item: ");
-        for(int i = 1; i < numberOfItems; i++){
-            items[1][i] = scanner.nextInt();
-        }
-        while(true){
-
-        }
-        //change here merge
+        System.out.println("The total profit is " + sum);
+        sc.close();
     }
 
 }
